d92942e77276f4ec695b083476f15237a451e313
LuK1337 <priv.luk@gmail.com>
2025-12-01 23:41:18 +0100

    Launcher3: ModelCallbacks: Remove screen id filter
    It seems like first screen is no longer added elsewhere?
    
    Change-Id: Iaed647bf16a802707cffdcb18573757530dae200

--- src/com/android/launcher3/ModelCallbacks.kt
+++ src/com/android/launcher3/ModelCallbacks.kt
@@ -340,7 +340,6 @@ class ModelCallbacks(private var launcher: Launcher) : BgDataModel.Callbacks {
             }
         }
         orderedScreenIds
-            .filter { screenId -> screenId != FIRST_SCREEN_ID }
             .forEach { screenId ->
                 launcher.workspace.insertNewWorkspaceScreenBeforeEmptyScreen(screenId)
             }
